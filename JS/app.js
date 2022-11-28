ce164a289cf86145c04681e7092941492fd1428c
Evening <evening@mstars-lab3-13s-iMac.local>
2022-11-28 17:45:49 +0800

    day-11

---
+++ JS/app.js
@@ -0,0 +1,40 @@
+console.log("Hello Word - 2");
+
+let ner = ("Tuvshin");
+let age = 25;
+let job ;
+const tall = null;
+let formatted = true;
+if (formatted) {
+console.log('Уншихад амар');
+}
+//Comments can make code readable
+console.log(ner);
+console.log(age);
+console.log(typeof(ner));
+console.log(typeof(age));
+console.log(typeof(formatted));
+console.log(typeof(job));
+console.log(typeof(tall));
+ //Javascript lesson begin
+ /*Comments can 
+ make code 
+ readable */
+
+ let a = 4;
+ let b = 5;
+ let c = 8;
+ console.log(" a*b/2 = ", a*b/2 );
+ console.log("(a+b+c) = ", a+b+c );
+ // m oloh
+ let y = 3;
+ let x = 6;
+ console.log("m=y**-y/x**-x = ", y**-y/x**-x );
+
+ // toirgin talbai oloh
+//  let r = 5;
+//  const p =3.14;
+//  console.log(S=p*r**);
+ let t=963;
+ let res = t%100;
+ console.log((t-res)/100)
