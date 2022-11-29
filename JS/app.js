3890619d0c7c7dfc2a7a1b6e2c857aac493fcc10
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2022-11-29 12:35:17 +0800

    home-11.30

--- JS/app.js
+++ JS/app.js
@@ -28,13 +28,94 @@ console.log(typeof(tall));
  console.log("(a+b+c) = ", a+b+c );
  // m oloh
  let y = 3;
- let x = 6;
+ let x = 7;
  console.log("m=y**-y/x**-x = ", y**-y/x**-x );
 
- // toirgin talbai oloh
-//  let r = 5;
-//  const p =3.14;
-//  console.log(S=p*r**);
- let t=963;
- let res = t%100;
- console.log((t-res)/100)
+ //toirgin talbai oloh
+ let r = 5;
+ const p =3.14;
+ console.log("S=p*r*r =", p*r*r);
+ //3 orontoi tooni anhnii toog ol
+ let v=583;
+ let res = v%100;
+ console.log((v-res)/100);
+//15gb heden bit we?
+let g=15;
+const k=1024;
+const bit=8;
+console.log("g*k3*bit = ", g*k*k*k*bit);
+
+//temperator
+let t=8;
+const z=1.8;
+const w=32;
+console.log("f=1,8*f+32 = ", z*t+w);
+console.log((t-w)/z)
+ //19 bodlogo
+ let f=15;
+ let e=10;
+ console.log("f+e =",f+e);
+ console.log("f-e =",f-e);
+ console.log("f/e =",f/e);
+ console.log("f%e =",f%e);
+ console.log("f*e =",f*e);
+
+ //cm inch hurwuuleh
+ let cm=9;
+ const ch=2.54;
+ console.log("1inch=2,54 = ", cm/ch);
+
+ //baby shark
+ let ba="Baby";
+ let s="Shark";
+ let o="doo";
+ let m="Mommy";
+ let d="Daddy";
+ let gm="Grandma";
+ let gp="Grandpa";
+ let lg="Let's go hunt";
+ let ra="Run away";
+ let sal="Safe at last";
+ let it="It's the end";
+
+ console.log(ba +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+ console.log(ba +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+ console.log(ba +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+ console.log(ba+" "+s);
+ console.log(m +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+ console.log(m +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+ console.log(m +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(m+" "+s);
+console.log(d +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(d +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(d +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(d +" "+s);
+console.log(gm +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(gm +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(gm +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(gm +" "+s);
+console.log(gp +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(gp +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(gp +" " +s + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(gp +" "+s);
+console.log(lg +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(lg +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(lg +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(lg);
+console.log(ra +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(ra +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(ra +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(ra);
+console.log(sal +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(sal +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(sal +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(sal);
+console.log(it +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(it +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(it +" "  + ", "+o+ "-"+o+", "+o+"-"+o);
+console.log(it);
+
+
+
+
+
