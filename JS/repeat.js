274ed1f51d18f7635e6629452bd00e22eab8a707
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2022-12-05 12:06:41 +0800

    home-12.05

--- JS/repeat.js
+++ JS/repeat.js
@@ -94,84 +94,90 @@ while(t>=1) {//235
 console.log(niilber);
 
 //10 bodlogo
-let x =0;
-let ax = 15;
-let sumAX = 0;
-while(x<= ax){
-    if(x % 2 !==0)
-    console.log(x);
-    x++;  
-    if((x % 2!==0)) 
-    sumAX += x;
-}
-console.log( `sumAX hurtelh sondgoi toonuudin niilber: ${sumAX}`);
-
-//11 bodlogo
-let z =0;
-let az = 15;
-let sumAZ = 0;
-while(z<= az){
-    if(z % 2 ==0)
-    console.log(z);
-    z++;  
-    if((z % 2==0)) 
-    sumAZ += z;
-}
-console.log( `sumAX hurtelh sondgoi toonuudin niilber: ${sumAZ}`);
-
-
-//12 bodlogo
-let u = 89776;
-// let len = u.lenght
-let urwuuT = 0;
-let unit = 0;
-while(u!==0){
-    unit = u % 10;
-    urwuuT = urwuuT * 10   + unit;
-    u = Math.floor(u/10);
-}
-console.log(urwuuT)
- // 0-100 hurtelh tegsh too
-// let tegshToo = 0;
-// while(tegshToo<=100){
-//     if(tegshToo % 2 === 0){
-//         console.log(tegshToo)
+// let x =0;
+// let ax =  prompt("Ta toogoo oruulna uu?");
+// let sumAX = 0;
+// while(x<= ax){
+//     if(x % 2 !==0){
+//         console.log(x);
 //     }
-//     tegshToo = tegshToo + 1;
+//     x++;  
+//     if((x % 2!==0)) {
+
+//         sumAX += x;
+//     }
 // }
+// console.log( `sumAX hurtelh sondgoi toonuudin niilber: ${sumAX}`);
 
-// 13 bodlogo
-const nmberS =[ "One", "two"]
- let num =764321;
- let p = 0, m =0, s=0;
- while(num !== 0){
-    p = num%10;
-    s = s*10 + p
-    if(p<10){
-        m +=p;
-    }
-    num = Math.floor(num/10);
- }
- console.log(s);
- let ug = " ";
+// //11 bodlogo
+// let z =0;
+// let az = prompt("Ta toogoo oruulna uu?");
+// let sumAZ = 0;
+// while(z<= az){
+//     if(z % 2 ==0){
 
- while(s !== 0){
-    p = s%10;
-    switch(p) {
-        case 0: {ug += "тэг " ; break}
-        case 1: {ug += " " +  " Нэг" ; break}
-        case 2: {ug += " " + "Хоёр " ; break}
-        case 3: {ug += " " + " Гурав" ; break}
-        case 4: {ug +=" " +  "Дөрөв" ; break}
-        case 5: {ug += " " + " Тав" ; break}
-        case 6: {ug += " " + "Зургаа "; break}
-        case 7: {ug += " " + " Долоо" ; break}
-        case 8: {ug += " " + " Найм" ; break}
-        case 9: {ug += " " + " Ес" ; break}
-    }
-    s = Math.floor(s/10);
- }
- console.log(ug);
+//         console.log(z);
+//     }
+//     z++;  
+//     if((z % 2==0)) {
+
+//         sumAZ += z;
+//     }
+//     }
+// console.log( `sumAX hurtelh sondgoi toonuudin niilber: ${sumAZ}`);
+
+
+// //12 bodlogo
+// let u = 89776;
+// // let len = u.lenght
+// let urwuuT = 0;
+// let unit = 0;
+// while(u!==0){
+//     unit = u % 10;
+//     urwuuT = urwuuT * 10   + unit;
+//     u = Math.floor(u/10);
+// }
+// console.log(urwuuT)
+//  // 0-100 hurtelh tegsh too
+// // let tegshToo = 0;
+// // while(tegshToo<=100){
+// //     if(tegshToo % 2 === 0){
+// //         console.log(tegshToo)
+// //     }
+// //     tegshToo = tegshToo + 1;
+// // }
+
+// // 13 bodlogo
+//  let num =764321;
+//  let p = 0, m =0, s=0;
+//  while(num !== 0){
+//     p = num%10;
+//     s = s*10 + p
+//     if(p<10){
+//         m +=p;
+//     }
+//     num = Math.floor(num/10);
+//  }
+//  console.log(s);
+//  let ug = " ";
+
+//  while(s !== 0){
+//     p = s%10;
+//     switch(p) {
+//         case 0: {ug += "тэг " ; break}
+//         case 1: {ug += " " +  " Нэг" ; break}
+//         case 2: {ug += " " + "Хоёр " ; break}
+//         case 3: {ug += " " + " Гурав" ; break}
+//         case 4: {ug +=" " +  "Дөрөв" ; break}
+//         case 5: {ug += " " + " Тав" ; break}
+//         case 6: {ug += " " + "Зургаа "; break}
+//         case 7: {ug += " " + " Долоо" ; break}
+//         case 8: {ug += " " + " Найм" ; break}
+//         case 9: {ug += " " + " Ес" ; break}
+//     }
+//     s = Math.floor(s/10);
+//  }
+//  console.log(ug);
  
 
 
@@ -225,5 +231,65 @@ for(b = 1; b<=10; b++){
 
 // square
 
-let q;
+let q = 5;
+let res = "";
+for(let t=1; t<=q; t ++ ){
+    for(let c =1; c <= q; c ++)
+    {
+        if(t === 1 || t === q || c ===1 || c===q){
+            res += "*" ;
+        }else{
+            res = res + " ";
+        }
+    }
+    res = res+ "\n"
+}
+console.log(res);
 
+// number digits
+let num = 7655999999999;
+let count = 0;
+while( num != 0){
+    num = Math.floor(num/10);
+    count ++
+}
+ console.log(count);
+
+// ugiig urwuulj unshih
+let text = "fghjk" ;
+let l = text.length - 1;
+let revText = ' ';
+while(l >=0){
+    revText = revText + text[l];
+    l--;
+}
+console.log(revText);
+
+// convert string
+let ug = "Hello WORld"
+let useg = " ";
+// console.log(ug.length)
+for (let q = 0; q < ug.length; q++){
+    let code = ug.charCodeAt(q);
+    if( code >= 65 && code <= 90 ){
+        useg += String.fromCharCode(code + 32);
+    } else if( code>= 96 && code<= 122 ){
+        useg += String.fromCharCode( code -32);
+    }
+    else {
+        useg += ug[q]
+    }
+}
+console.log(useg )
+ // fibonacci
+let s = 8;
+ let p = 0, a = 1, sumF;
+ for (k = 0; k<=s; k++){
+    console.log(p);
+    sumF = p + a;
+    p=a;
+    a = sumF
+ }
+ console.log( sumF);
+
+ //random too oloh
