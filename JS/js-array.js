1685ef65e1fd5eeee060e9c237571ff1ef0c26b3
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2022-12-06 11:56:22 +0800

    home....

--- JS/js-array.js
+++ JS/js-array.js
@@ -45,16 +45,22 @@ console.log(avarage);
 // console.log(min)
 
 // // array ehend durin 1 too nemeh
-// let arr = [ 43, 56, 23, 89, 88, 90, 99, 11, 652, 15, 290];
-// arr.pop();
-// console.log(arr)
-// arr.push(28);
-// console.log(arr)
-// arr.unshift(100)
-// console.log(arr)
-// arr.shift()
-// console.log(arr)
+let arr = [ 43, 56, 23, 89, 88, 90, 99, 11, 652, 15, 290];
+let sumN =0;
+for(let d = 0; d<= arr.length-1; d++){
+   sumN += arr[d];
+}
+console.log(sumN);
     
+arr.pop();
+console.log(arr)
+arr.push(28);
+console.log(arr)
+arr.unshift(100)
+console.log(arr)
+arr.shift()
+console.log(arr)
+
 //  //simple array -1
  let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple',   'IBM', 'Oracle', 'Amazon'];
  console.log(itCompanies)
@@ -64,11 +70,70 @@ console.log(avarage);
  for( n=0; n <= itCompanies.length-1; n++){
     console.log(itCompanies[n]);
 }
+ for( a=0; a <= itCompanies.length-1; a++){
+    console.log(itCompanies[a].toUpperCase());
+}
 
  console.log("facebook index:", itCompanies.indexOf("Facebook"))
 console.log(itCompanies[0]);
  console.log(itCompanies[Math.floor(itCompanies.length/2) ]);
  console.log(itCompanies[itCompanies.length - 1] );
- console.log(itCompanies[0][0]);
- console.log(itCompanies[2].toUpperCase());
+ console.log(itCompanies + " " + 'зэрэг мэдээллийн технологийн томоохон компаниуд.')
+//  console.log(itCompanies[0][0]);
+//  console.log(itCompanies[2].toUpperCase());
 
+let cutX = itCompanies.slice(0, 3 );
+console.log(cutX);
+let cutL = itCompanies.slice(4, 7)
+console.log(cutL);
+let cutA = itCompanies.slice (3, 5)
+console.log(cutA)
+console.log(itCompanies)
+itCompanies.shift();
+console.log(itCompanies)
+itCompanies.pop();
+console.log(itCompanies);
+ let data = ['Засгийн ', 'газарт', '6,4', 'сая', 'тонн', 'нүүрс', 'алдагдсан', 'гэх', ' мэдээлэл', 'ирээгүй', 'байна.', 'Бодит', 
+ 'байдалд', 'ийм', 'их', 'хэмжээний ', 'нүүрс', 'алдагдсан', 'гэх', 'асуудал', 'эргэлзээтэй', 'байна.', 'Хууль', 'хяналтын',
+ 'байгууллага', 'хяналт', 'шалгалтын', 'ажил', 'явуулж',  'байна'];
+ let textN = 0;
+ while (textN <= data.length - 1 ) {
+if (data[textN][0] === 'н' ) {
+console.log(data[textN]);
+}
+textN++;
+}
+
+
+let arrNumber = [5, 6, 4, 12, 19, 121, 1, 7, 9, 63];
+let evenN = [], oddN = [];
+for( let b = 0; b < arrNumber.length; b++){
+   if(arrNumber[b] % 2 ===0){
+      evenN.push(arrNumber[b]);
+   } else if (arrNumber[b] %2 !== 0){
+      oddN.push(arrNumber[b]);
+   }
+}
+console.log(evenN);
+console.log(oddN);
+
+let x = [1, 2, 3, 4, 5]
+let q = '';
+for(let y = 0; y < x.length; y++){
+//    for(let z=0; z<=y; z++){
+//    //    q = q + x[y] + " "
+// }
+    q= q + ' ' + x[y]
+   console.log(q);
+}
+
+let arr1 = [ [3, 45, 23, 78,34],
+             [4, 2, 34, 4, 12,1]];
+let urjver = [];
+for(let e=0; e<arr1.length; e++){
+   for (let j = 0; j<arr1[e]; e++){
+      urjver = arr1[e][[j]]*
+      urjver.push(arr1[e][j])
+   }
+}
+console.log(urjver)
