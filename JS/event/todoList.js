125aa9493c8a39cff2cd09183b4a4536621ff992
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2022-12-15 12:15:32 +0800

    home ....

--- JS/event/todoList.js
+++ JS/event/todoList.js
@@ -1,7 +1,8 @@
 
- const button = document.getElementsByTagName('button')[0];
+//  const addBtn = document.getElementsByTagName('button')[0];
+const addBtn =  document.getElementById('add-btn')
  const taskName = document.getElementById('taskName');
- let todo= document.getElementById("groupTodo");
+ let grouptodo = document.getElementById("groupTodo");
 //  const listed = () =>{
 //     let el = `<div class="todo-list">
 //         <input type="text" name="" id="task">
@@ -18,31 +19,72 @@
 //     el_section.getElementsByTagName("input")[0].value = inp
 //  }
 //  button.addEventListener('click', listed)
+const ognoo = document.getElementsByTagName('span')[0];
+const yearDay = () =>{
+     day = new Date().getDay();
+     month = new Date().getMonth();
+     year = new Date().getFullYear();
 
-const cardItem =(text) =>{
-        let item = `<div class="todo-list">
-        <input type="text" name="" id="task readonly value = "${text}">
-        <div class="icons">
-            <i class="fa-sharp fa-solid fa-pen" style="color: black;"></i>
-            <i class="fa-sharp fa-solid fa-check" style="color: green;"></i>
-            <i class="fa-sharp fa-solid fa-trash" style="color: red;"></i>
-        </div>
-</div>`;
-return item;
-// console.log(item)
-};
- const  taskAdd =() =>{
+     ognoo.textContent = `${year}-${month}-${day}`
+}
+setInterval(yearDay);
+const ustgah = (e) => {
+    alert("Ta ustgahdaa itgeltei bna uu?");
+    const parent = e.parentNode.parentNode.parentNode;
+    const child = e.parentNode.parentNode;
+    parent.removeChild(child)
+}
+
+const cardItem = (text) =>{ 
+    const item = 
+    `<div class="todo-list">
+            <input type="text" name="" id="task" readonly value="${text}">
+            <div class="icons">
+            <button class="edit" onclick = "edit(this)" >  <i class="fa-sharp fa-solid fa-pen" style="color: black;"></i> </button>                
+            <button class="check" onclick = "checkBox(this)"  ><i class="fa-sharp fa-solid fa-check" style="color: green;"></i></button>
+            <button class="delete" onclick = "ustgah(this)" ><i class="fa-sharp fa-solid fa-trash" style="color: red;"></i></button>
+            </div>
+    </div>`;
+    return item;
+}
+const taskAdd = () => {
     let text = taskName.value;
     if(text === ""){
-        alert('Ta utga oruulaagui bna');
-    } else{
-        todo.innerHTML += cardItem(text)
+        alert("Ta utga oruulaagui baina");
+    }else{
+        grouptodo.innerHTML += cardItem(text);
     }
 }
-console.log(todo)
-  button.addEventListener('click', taskAdd);
-  document.addEventListener('keyup', (e)=>{
-    if(e.key === 'Enter'){
+addBtn.addEventListener("click",taskAdd);
+document.addEventListener('keyup',(e)=>{
+    if(e.key === "Enter"){
         taskAdd();
     }
-  })
+});
+ 
+//checked
+const checkBox =() =>{
+    let checked = check;
+    let red = 'red';
+    let green = 'green'
+    if(checked === "checked"){
+        checked.style.color = `${green}`
+    }else{
+        checked.style.color = `${red}`
+
+    }
+    return checked ;
+}
+
+
+// const checked = document.createElement('div');
+// div.innerHTML = checkBox
+// const checkBox = ()=>{
+//     let span = document.createElement("SPAN");
+//     let txt =document.createTextNode("\u00D7")
+//     span.className = 'close';
+//     span.appendChild(txt);
+// }
+
+
+ 
