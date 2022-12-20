6bf481cd3b16c3bffb2aac0c7b9280f4036c7004
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2022-12-20 12:15:24 +0800

    home.....20

--- JS/DOM/number/script.js
+++ JS/DOM/number/script.js
@@ -1,29 +1,45 @@
 const btnAgain = document.getElementsByClassName('btn again')[0];
-console.log(btnAgain);
 const number = document.getElementsByClassName('number')[0];
-console.log(number);
-const guessNumber = document.getElementsByClassName('guess')[0];
-// console.log(guessNumber);
-// console.log(typeof(guessNumber));
+const guess = document.getElementsByClassName('guess')[0];
 const btnCheck = document.getElementsByClassName('btn check')[0];
-console.log(btnCheck);
+const body = document.getElementsByTagName('body')[0]
+const score = document.getElementsByClassName('score')[0];
+const highscore = document.getElementsByClassName('highscore')[0];
+// // btnCheck.addEventListener('click', ()=>{
+//     number.innerHTML = Math.floor(Math.random() * 20);
+// })
+// btnAgain.addEventListener('click', ()=>{
+//     highscore.innerHTML = success;
+// })
 
-btnAgain.addEventListener('click', ()=>{
-    number.innerHTML = Math.floor(Math.random() * 101);
-})
- 
+let onoo = 20;
+let success = 0;
+let guessNumber = () =>{
+    let  x =  (number.innerHTML = Math.floor(Math.random()*21));
+    let e= guess.value;
+    if(e==x){
+        alert("Ta zuw taalaa ");
+        body.style.backgroundColor = '#60b347';
+        onoo++
+        success++
+    } else if(x<e){
+        alert(' ih too bna');
+        body.style.backgroundColor = '#222';
+        onoo --;
+        success =0;
 
-
-// let inpNumber = () =>{
-//     let a = parseInt(prompt('ta toogoo oruulna uu?'))
-//     console.log(typeof(a));
-//        guessNumber.value = a;
-//        let n= typeof(guessNumber.value)
-//        console.log(typeof(n));
-//     if(  a<20 && a>=0 ){
-//         alert('Zuw bna')
-//     }else{
-//         alert('Too oruulna uu?')
-//     }
-// }
-// btnCheck.addEventListener('click', inpNumber);
+    }else{
+        alert('baga too bna');
+        body.style.backgroundColor = '#222';
+        onoo --;
+        success = 0;
+    }
+    score.innerHTML = onoo;
+    if(onoo==0){
+        alert('ta hojigdloo');
+    }
+    highscore.innerHTML = success;
+}
+btnCheck.addEventListener('click', guessNumber);
+// btnCheck.addEventListener('click', x)
+// console.log(x);
--- JS/javaScript/string.js
+++ JS/javaScript/string.js
@@ -19,4 +19,106 @@ console.log(typeof(regUseg)=="string", Number(regToo))
 console.log(reg.charCodeAt(0));
 if(reg.charCodeAt(0)>=65&&reg.charCodeAt(0)<92){
 }
-//fieldset
+//nearest number
+// let n = parseInt(prompt('Ta tooogoo oruulna uu?'))
+// console.log(Math.round(n));
+//number sum
+let n = 423;
+let sum =0;
+while(n>=1){
+    sum =sum + (n%10);
+    n = Math.floor(n/10)
+}
+console.log(sum);
+//random number between
+// let a = parseInt(prompt('Ta toogoo oruulna uu'));
+// let b = parseInt(prompt('Ta toogoo oruulna uu'));
+// if(a>b){
+//     max=a;
+//     min=b
+// }else{
+//     max = b;
+//     min = a
+// }
+// let c = Math.random() *(max - min)+min;
+// console.log(Math.round(c));
+
+//simple assignment
+let  r = Math.random()*101;
+console.log(r);
+
+// Validate phone number
+// let phoneN = prompt("Ta phone number oruulna uu?");
+// let phoneN = prompt('Ta utasnihaa dugaarig oruulna uu');
+// let utasniDugaar = ''
+// if(Number(phoneN)){
+//     utasniDugaar = phoneN
+// } else{
+//     utasniDugaar = 'Ta utasni dugaara zuw oruulna uu'
+// }
+// console.log(utasniDugaar);
+
+// let phonenumber = '';
+// let i = +976;
+// let j = phoneN.substring(0, 4);
+// if(i==j && phoneN.length === 12){
+//     phonenumber = 'Ta mongol ulsin utasni dugaarig zuw oruulsan bn'
+// }else{
+//     phonenumber = 'Ta mongol ulsin utasni dugaarig buruu oruulsan bn'
+// }
+// console.log(phonenumber);
+
+// JS string
+// escape sequences
+let h = 'Hello I am "Enkhtuvshin"';
+let g = 'I am Age here years old'
+let w = ' This is the new line with tab and some \\';
+console.log(h);
+console.log(g);
+console.log(w);
+
+// phone number hewleh
+// let ph= prompt('Phone number');
+// let p = ph.substring(0, 2);
+// let operator = ''
+// if(p==99 || p==95){
+//     operator = 'mobicom'
+// }else if(p==88 || p==86 || p==80){
+//     operator = 'unitel'
+// }else if(p==98 || p==93 || p==78){
+//     operator = 'gmobile'
+// }else if(p==96 || p==90 || p==91){
+//     operator = 'skytel'
+// }else{
+//     operator='Utasni dugaara buruu bna '
+// }
+// console.log(operator);
+
+//ognoo hewleh
+// let on = prompt('Ta onoo oruulna  uu?');
+// let sar = prompt('Ta saraa oruulna uu?');
+// let day = prompt('Ta udruu oruulna uu?');
+// console.log(sar.length);
+// if(day.length==1 ){
+//    day = 00 + day
+// }
+//  if(sar.length == 1) {
+//     sar = 00 + sar
+// }
+// else{
+//     day= day
+//     sar=sar
+// }
+// console.log(on+'-'+sar+'-'+day);
+
+// simple assignment
+let firstname = 'Enkhtuvshin';
+let lastname = 'Dashnyam';
+console.log(firstname.length+":"+lastname.length);
+let fullname = lastname.concat('' , ' Enkhtuvshin')
+console.log(fullname);
+console.log(fullname.toLocaleLowerCase());
+console.log(fullname.toLocaleUpperCase());
+
+let a = 12345
+console.log(escape(a));
