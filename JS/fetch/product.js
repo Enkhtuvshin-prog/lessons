3d1705284b3e4499994ca3c5138042df2084fd5c
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2022-12-27 12:26:20 +0800

    home   .26

--- JS/fetch/product.js
+++ JS/fetch/product.js
@@ -2,6 +2,11 @@ console.log('product');
 const productList = document.querySelector('.productList');
 let allProducts= [];
 
+const cart =(e) =>{
+  const parent = e.parentNode.parentNode.parentNode
+  console.log(parent);
+}
+
 const displayProduct = () =>{
     allProducts.forEach((product) =>{
         const item = 
@@ -28,9 +33,9 @@ const displayProduct = () =>{
             <p>${product.rating}</p>
             
           </div>
-          <div class="col">
-            <button class="btn btn-outline-primary">
-              <img src="./img/zurh.png" alt=""><span class="ms-2">Watch</span>
+          <div class="col ms-auto">
+            <button class="btn btn-outline-primary" onclick="cart(this)">
+             Сагсанд нэмэх
             </button>
           </div>
         </div>
@@ -40,42 +45,8 @@ const displayProduct = () =>{
   `
   productList.innerHTML += item
     } 
- );
-//     productList.innerHTML = allProducts.map((product) =>
-//     `
-//     <div class="col-12 col-md-6 col-xl-3 ">
-//     <div class="card h-100">
-//       <img src="${product.thumbnail}" class="card-img-top h-50" alt="...">
-//       <div class="card-body">
-//         <h5 class="card-title text-center">${product.title}</h5>
-//         <div class="badge bg-success-subtle  ms-auto" style="width: 6rem; color:green;">
-//           ${product.discountPercentage}OFF.
-//         </div>
-//         <div class="text">
-//           <h3>$${product.price}</h3>
-//           <p class="text-truncate">${product.description}</p>
-//         </div>
-//         <div class="row ">
-//           <div class="col">
-//             <img src="./img/Vector.png" alt="">
-//             <img src="./img/Vector.png" alt="">
-//             <img src="./img/Vector.png" alt="">
-//             <img src="./img/Vector.png" alt="">
-//             <img src="./img/Vector.png" alt="">
-//             <p>${product.rating}</p>
-            
-//           </div>
-//           <div class="col">
-//             <button class="btn btn-outline-primary">
-//               <img src="./img/zurh.png" alt=""><span class="ms-2">Watch</span>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-//  `)
-}
+ )
+};
  
 const getProducts= async () =>{
     const response = await fetch('https://dummyjson.com/products');
@@ -85,3 +56,4 @@ const getProducts= async () =>{
     console.log(data);
 }
 getProducts();
+
